a28b75093e821851d7eea3bdda1487b5a47c62ef
Thomas <thomas@lekoala.be>
2022-07-25 15:58:25 +0200

    improve normalize data

--- src/utils/normalizeData.js
+++ src/utils/normalizeData.js
@@ -1,25 +1,34 @@
 "use strict";
 
 /**
- * Similar to bootstrap 5 but also parse JSON
+ * Parse data attribute and return properly typed data
  * @param {string} val
+ * @returns {mixed}
  */
 export default function normalizeData(val) {
+  // Bool
   if (val === "true") {
     return true;
   }
   if (val === "false") {
     return false;
   }
+  // Null or empty
   if (val === "" || val === "null") {
     return null;
   }
+  // Numeric attributes
   if (val === Number(val).toString()) {
     return Number(val);
   }
+  // Only attempt json parsing for array or objects
   if (val.indexOf("[") === 0 || val.indexOf("{") === 0) {
     try {
-      return JSON.parse(val.replaceAll("'", '"'));
+      // In case we have only single quoted values
+      if (val.indexOf('"') === -1) {
+        val = val.replaceAll("'", '"');
+      }
+      return JSON.parse(decodeURIComponent(val));
     } catch {
       console.log("Failed to parse " + val);
       return {};
